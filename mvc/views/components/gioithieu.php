31a3a8d7205a1d5e0a4a1403e10737b9561c1e4e
Nguyễn Trọng Phúc <ngtrongphuc1905@gmail.com>
2021-11-27 19:13:40 +0700

    Update gioithieu.php

--- mvc/views/components/gioithieu.php
+++ mvc/views/components/gioithieu.php
@@ -19,15 +19,16 @@
                         <div class="col-lg-6 order-last order-lg-first">
                             <div class="about-text-wrap">
                                 <h2>Giới thiệu</h2>
-                                <p>Công ty Cổ phần Đầu tư Thế Giới Di Động (MWG) là nhà bán lẻ số 1 Việt Nam về doanh thu và lợi nhuận, với mạng lưới hơn 4.500 cửa hàng trên toàn quốc. MWG vận hành các chuỗi bán lẻ thegioididong.com, Điện Máy Xanh, Bách Hoá Xanh. Ngoài ra, MWG đã mở rộng ra thị trường nước ngoài với chuỗi bán lẻ điện thoại và điện máy tại Campuchia và đầu tư vào chuỗi nhà thuốc An Khang.
-                                    CÁC THƯƠNG HIỆU THUỘC MWG</p>
+                                <p>Cửa hàng điện thoại di động G.I.Joe là chi nhánh thuộc tập doàn BKU PhoneStore, tọa lạc tại vị trí đắc địa ở trung tâm Quận 10, Tp. Hồ Chí Minh. Chúng tôi cam kết mang đến những sản phẩm chất lượng, giá cả cạnh tranh nhất trên thị trường hiện nay.</p>
+                                <br>
+                                <p>Với phương châm: "Tất cả vì niềm tin của Khách hàng". Chúng tôi luôn cố gắng đem lại cho khách hàng những gì tốt nhất!</p>
                             </div>
                         </div>
                         <!-- About Text End -->
                         <!-- About Image Start -->
                         <div class="col-lg-5 col-md-10">
                             <div class="about-image-wrap">
-                                <img class="img-full" src="https://cdn.cellphones.com.vn/media/catalog/product/cache/7/image/1000x/040ec09b1e35df139433887a97daa66f/v/s/vsmart-star-5-3_3.jpg" alt="About Us" />
+                                <img class="img-full" src="https://cdn3.iconfinder.com/data/icons/digital-marketing-filled/64/Digital_Marketing-21-512.png" alt="About Us" />
                             </div>
                         </div>
                         <!-- About Image End -->
